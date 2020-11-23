17f5736263d83c762634ba1ecb64a25d16679cb5
Vladimir Sokolov <vsokol74@main.ru>
2020-11-23 13:34:25 +0300

    + библиотеки тестирования

---
+++ HomeWork23/src/test/java/store/sokolov/innopolis/homework_23/task_1_2/TestJUnitTest.java
@@ -0,0 +1,50 @@
+package store.sokolov.innopolis.homework_23.task_1_2;
+
+import org.junit.jupiter.api.*;
+
+class TestJUnitTest {
+
+    @BeforeAll
+    static void setUpAll() {
+    }
+
+    @AfterAll
+    static void tearDownAll() {
+    }
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void main() {
+    }
+
+    @Test
+    void task1() {
+    }
+
+    @Test
+    void task2() {
+    }
+
+    @Test
+    void task3() {
+    }
+
+    @Test
+    void task4() {
+    }
+
+    @Test
+    void task5() {
+    }
+
+    @Test
+    void printCheckedObject() {
+    }
+}
